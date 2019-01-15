44d3022b849c950c128a22e6937d071b45df48d7
keeper-louis <keeper_louis@sina.com>
2019-01-15 15:33:44 +0800

    add 3D框架 by keeper at 2019/01/15

---
+++ 3DHomeProject/KEEPER.K3.3D.Core/ParamOption/BillStatusEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEEPER.K3._3D.Core.ParamOption
+{
+    public enum BillStatusEnum
+    {
+        Save,
+        Audit
+    }
+}
---
+++ 3DHomeProject/KEEPER.K3.3D.Core/ParamOption/ConvertOption.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEEPER.K3._3D.Core.ParamOption
+{
+    public class ConvertOption
+    {
+        /// <summary>
+        /// 源单标识
+        /// </summary>
+        public string SourceFormId { get; set; }
+
+        /// <summary>
+        /// 目标单标识
+        /// </summary>
+        public string TargetFormId { get; set; }
+
+        /// <summary>
+        /// 单据转换规则KEY
+        /// </summary>
+        public string ConvertRuleKey { get; set; }
+    }
+}
