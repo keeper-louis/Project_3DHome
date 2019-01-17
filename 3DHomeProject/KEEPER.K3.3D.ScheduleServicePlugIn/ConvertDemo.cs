4e69b5e9e757b563d80a6e486ed3ae19e5474407
keeper-louis <keeper_louis@sina.com>
2019-01-17 18:40:56 +0800

    add ConvertDemo by keeper at 2019/1/17

---
+++ 3DHomeProject/KEEPER.K3.3D.ScheduleServicePlugIn/ConvertDemo.cs
@@ -0,0 +1,39 @@
+﻿using Kingdee.BOS.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kingdee.BOS;
+using Kingdee.BOS.Core;
+using System.ComponentModel;
+using KEEPER.K3._3D.Core.ParamOption;
+
+namespace KEEPER.K3._3D.ScheduleServicePlugIn
+{
+    [Description("自动下推汇报执行计划demo")]
+    public class ConvertDemo : IScheduleService
+    {
+        public void Run(Context ctx, Schedule schedule)
+        {
+            ConvertOption option = new ConvertOption();
+            option.SourceFormId = "SFC_OperationPlanning";
+            option.TargetFormId = "SFC_OperationReport";
+            List<long> sourceBillIds = new List<long>();
+            sourceBillIds.Add(1016279);
+            option.SourceBillIds = sourceBillIds;
+            List<long> sourceBillEntryIds = new List<long>();
+            sourceBillEntryIds.Add(4135730);
+            sourceBillEntryIds.Add(4135731);
+            sourceBillEntryIds.Add(4135732);
+            sourceBillEntryIds.Add(4135733);
+            sourceBillEntryIds.Add(4135734);
+            sourceBillEntryIds.Add(4135735);
+            sourceBillEntryIds.Add(4135736);
+            sourceBillEntryIds.Add(4135737);
+            sourceBillEntryIds.Add(4135738);
+            option.SourceBillEntryIds = sourceBillEntryIds;
+            option.SourceEntryEntityKey = "FSubEntity";
+        }
+    }
+}
