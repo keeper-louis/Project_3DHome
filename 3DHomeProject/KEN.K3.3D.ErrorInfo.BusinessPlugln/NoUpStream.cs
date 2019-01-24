0bd5808665b854545039401fb1f4f62942a1e472
keeper-louis <keeper_louis@sina.com>
2019-01-24 12:15:42 +0800

    add by wdh at 2019/1/24 NoUpStream

--- 3DHomeProject/KEN.K3.3D.ErrorInfo.BusinessPlugln/NoUpStream.cs
+++ 3DHomeProject/KEN.K3.3D.ErrorInfo.BusinessPlugln/NoUpStream.cs
@@ -1,6 +1,7 @@
 ﻿using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
 using Kingdee.BOS.Core.List;
 using Kingdee.BOS.Core.List.PlugIn;
+using Kingdee.BOS.Orm.DataEntity;
 using Kingdee.BOS.Util;
 using System;
 using System.ComponentModel;
@@ -10,32 +11,33 @@ using System.Linq;
 namespace KEN.K3._3D.ErrorInfo.BusinessPlugln
 
 {
-    public class NoUpStream : AbstractListPlugIn
+    public class ErrorInfoBill : AbstractListPlugIn
     {
-        [Description("处理无上游单据")]
+        [Description("错误信息表列表插件")]
         public override void BarItemClick(BarItemClickEventArgs e)
         {
             ListSelectedRowCollection selectRows = this.ListView.SelectedRowsInfo;
-            string objectID = this.ListView.BillBusinessInfo.GetForm().Id;
-            //string entiryKey = this.ListView.Model.BillBusinessInfo.GetField("FQty").EntityKey;
-
-            switch (e.BarItemKey.ToUpperInvariant())
+            //处理无上游单据
+            if (String.Equals(e.BarItemKey.ToUpperInvariant(), "NoUpStream", StringComparison.CurrentCultureIgnoreCase))
             {
-                case "TBBOMCONFIG":
-                    //分是否显示单据体处理
-                    if (selectRows.Count() != 1 || this.ListView.SelectedRowsInfo[0].EntryEntityKey != "FSaleOrderEntry"
-                        || this.ListView.SelectedRowsInfo[0].EntryPrimaryKeyValue.IsNullOrEmptyOrWhiteSpace())
-                    {
-                        e.Cancel = true;
-                        this.View.ShowMessage("请选择唯一一行单据分录进行配置操作！");
-                        break;
-                    }
+                string filter = string.Empty;
+                for (int i = 0; i < selectRows.Count(); i++)
+                {
+                    filter = filter + Convert.ToString(selectRows[i].DataRow["FPrtableinID"]) + ",";
+                }
+                filter = filter.TrimEnd(',');
 
-                    //私有函数，进行BOM配置，具体逻辑略去
-                    //BomConfigViewEdit();
-                    break;
-                    //其他case分支处理逻辑略去
+
+                //分是否显示单据体处理
+                if (selectRows.Count() != 1 || this.ListView.SelectedRowsInfo[0].EntryEntityKey != "FSaleOrderEntry"
+                    || this.ListView.SelectedRowsInfo[0].EntryPrimaryKeyValue.IsNullOrEmptyOrWhiteSpace())
+                {
+                    e.Cancel = true;
+                    this.View.ShowMessage("请选择唯一一行单据分录进行配置操作！");
+
+                }
             }
+
         }
     }
 }
